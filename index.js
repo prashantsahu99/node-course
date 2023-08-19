776de29327b34110562b8154585ca947a0adf906
prashantsahu99 <prashantsahu9920@gmail.com>
2023-08-19 10:37:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const validator = require('validator');
+const getNotes = require('./notes');
+console.log(getNotes());
+console.log(validator.isURL('https://nodejs.org'));
---
+++ notes.js
@@ -0,0 +1,2 @@
+const getNotes = ()=> {return 'Your Notes...'};
+module.exports = getNotes;
